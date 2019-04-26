22035ac27bb1b8e10c2fd15d16fd1c130be7717f
Anupama Dhir <sabhssabhs@yahoo.com>
2019-04-26 11:05:35 +0800

    Added menu

--- Recipe.py
+++ Recipe.py
@@ -31,61 +31,76 @@ class Recipe:
      def get_category(self):
          return self.category
 
-while entering:
-    recipeName = input("Enter recipe name: ")                
-    recipe_category = input("Enter recipe category: ") 
-    recipe = Recipe(recipeName, recipe_category)  
-    print("Enter the ingredients")
-    try:
-        while True:
-            inp = input()
-            if inp != "":
-                recipe.add_ingredient(inp)
-            else:
-                break
-    except EOFError:
+def Menu():
+    print ("Menu:")
+    print ("1. Enter new recipe")
+    print ("2. List all recipes")
+    print ("3. List recipes by Category (Starter/ Main Course/ Dessert)")
+    print ("4. Quit")
+    return (int(input("Enter your choice.")))
+
+while True:
+    menu_selection = Menu()
+    if menu_selection == 1:
+        recipeName = input("Enter recipe name: ")                
+        recipe_category = input("Enter recipe category: ") 
+        recipe = Recipe(recipeName, recipe_category)  
+        print("Enter the ingredients")
+        try:
+            while True:
+                inp = input()
+                if inp != "":
+                    recipe.add_ingredient(inp)
+                else:
+                    break
+        except EOFError:
+            pass
+        
+        display_ingredients = recipe.getIngredients()
+        print ("Ingredients:")
+        print(display_ingredients)
+        display_category = recipe.get_category()
+        print("Category:")
+        print(display_category)
+        #list_dessert.append(recipeName)
+        if display_category == 'Dessert':
+            dict_dessert[recipeName] = display_ingredients
+            print ("Dessert Details")
+            print(dict_dessert)
+            list_dessert.append(recipeName)
+            dict_categories[display_category] = list_dessert
+        elif display_category == 'Main Course':
+            dict_maincourse[recipeName] = display_ingredients
+            print ("Main Course Details")
+            print(dict_maincourse)
+            list_maincourse.append(recipeName)
+            dict_categories[display_category] = list_maincourse
+        elif display_category == 'Starter':
+            dict_starter[recipeName] = display_ingredients
+            print ("Starter Details")
+            print(dict_starter)
+            list_starter.append(recipeName)
+            dict_categories[display_category] = list_starter
+        else:
+            print ("Category doesn't exist")
+        print (dict_categories)
+    elif menu_selection == 2:
         pass
-    
-    display_ingredients = recipe.getIngredients()
-    print ("Ingredients:")
-    print(display_ingredients)
-    display_category = recipe.get_category()
-    print("Category:")
-    print(display_category)
-    #list_dessert.append(recipeName)
-    if display_category == 'Dessert':
-        dict_dessert[recipeName] = display_ingredients
-        print ("Dessert Details")
-        print(dict_dessert)
-        list_dessert.append(recipeName)
-        dict_categories[display_category] = list_dessert
-    elif display_category == 'Main Course':
-        dict_maincourse[recipeName] = display_ingredients
-        print ("Main Course Details")
-        print(dict_maincourse)
-        list_maincourse.append(recipeName)
-        dict_categories[display_category] = list_maincourse
-    elif display_category == 'Starter':
-        dict_starter[recipeName] = display_ingredients
-        print ("Starter Details")
-        print(dict_starter)
-        list_starter.append(recipeName)
-        dict_categories[display_category] = list_starter
-    else:
-        print ("Category doesn't exist")
-    print (dict_categories)
-
-    
-    y_or_n = input("Do you want to enter another recipe? y or n ")
-
-    if y_or_n[0].lower() == 'y':
-
-        entering = True
-
-    else:
-
-        entering = False
-
-        print("No more recipes to add")
-
+    elif menu_selection == 3:
+        pass
+    elif menu_selection == 4:
         break
+    
+#    y_or_n = input("Do you want to enter another recipe? y or n ")
+#
+#    if y_or_n[0].lower() == 'y':
+#
+#        entering = True
+#
+#    else:
+#
+#        entering = False
+#
+#        print("No more recipes to add")
+#
+#        break
